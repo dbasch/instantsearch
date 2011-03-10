3da94be2d5e219acd89b0fdbf36ae94f79c8a6c2
Adrian <adrian@indextank.com>
2011-03-10 19:38:03 -0300

    Add verbosity and begin rescue end for connection problems

--- indexer.rb
+++ indexer.rb
@@ -6,12 +6,17 @@ CONFIG = YAML.load_file 'config.yml'
 api_client = IndexTank::Client.new CONFIG['api_url']
 idx = api_client.indexes CONFIG['index']
 
+puts "Indexing..."
 f = File.open("instrument.tsv")
 f.readline #discard header
 count = 0
 f.each_line do |l|
-  name,id,family,variation,instrumentalists  = l.split /\t/ 
-  idx.document(id).add({:text => l, :name => name, :family => family, :variation => variation})
+  name,id,family,variation,instrumentalists  = l.split /\t/
+  begin
+    idx.document(id).add({:text => l, :name => name, :family => family, :variation => variation})
+  rescue
+    puts "error: " + l
+  end
   count += 1
   if count % 50 == 0
     printf "Added %s documents\n", count
