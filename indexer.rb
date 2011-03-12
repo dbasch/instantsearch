adffa00a91bfcd4cb168e3e8bc333aeebef769cc
Diego Basch <dbasch@Diego-Baschs-Macbook.local>
2011-03-11 16:04:44 -0800

    changed to use batch indexing

--- indexer.rb
+++ indexer.rb
@@ -10,16 +10,9 @@ idx = api_client.indexes CONFIG['index']
 puts "Indexing..."
 f = File.open("instrument.tsv")
 f.readline #discard header
-count = 0
+docs = Array.new
 f.each_line do |l|
   name,id,family,variation,instrumentalists  = l.split /\t/
-  begin
-    idx.document(id).add({:text => l, :name => name, :family => family, :variation => variation})
-  rescue
-    puts "error: " + l
-  end
-  count += 1
-  if count % 50 == 0
-    printf "Added %s documents\n", count
-  end
+  docs << {:docid => id, :fields => {:text => l, :name => name, :family => family, :variation => variation}}
 end
+printf "Indexed %d documents.\n", idx.batch_insert(docs).size
